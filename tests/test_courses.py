4933a85862a2ee1edb50744bf2c956b300d295c5
serlab-nn <me@labaev.ru>
2025-04-16 21:35:24 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

---
+++ tests/test_courses.py
@@ -0,0 +1,47 @@
+import time
+
+import pytest
+from playwright.sync_api import sync_playwright, expect
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(
+        create_course_page: CreateCoursePage,
+        courses_list_page: CoursesListPage,
+        chromium_page_with_state
+):
+    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form(title='', description='', estimated_time='', max_score='0', min_score='0')
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image('./testdata/files/image.png')
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.fill_create_course_form(
+        title="Playwright",
+        estimated_time="2 weeks",
+        description="Playwright",
+        max_score="100",
+        min_score="10"
+    )
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+            index=0,
+            title='Playwright',
+            max_score='100',
+            min_score='10',
+            estimated_time='2 weeks'
+    )
+
+
+
+
+
+
