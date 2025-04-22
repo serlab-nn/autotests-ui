a962d4b7fe5d3954f53f0295b591373b51214294
serlab-nn <me@labaev.ru>
2025-04-22 20:34:26 +0300

    Знакомство с PageComponent. Практика работы с Page Component

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -13,14 +13,14 @@ class SidebarListItemComponent(BaseComponent):
         self.title = page.get_by_test_id(f'{identifier}-drawer-list-item-title-text')
         self.button = page.get_by_test_id(f'{identifier}-drawer-list-item-button')
 
-        def check_visible(self, title: str):
-            expect(self.icon).to_be_visible()
+    def check_visible(self, title: str):
+        expect(self.icon).to_be_visible()
 
-            expect(self.title).to_be_visible()
-            expect(self.title).to_have_text(title)
+        expect(self.title).to_be_visible()
+        expect(self.title).to_have_text(title)
 
-            expect(self.button).to_be_visible()
+        expect(self.button).to_be_visible()
 
-        def navigate(self, expected_url: Pattern[str]):
-            self.button.click()
-            self.check_current_url(expected_url)
+    def navigate(self, expected_url: Pattern[str]):
+         self.button.click()
+         self.check_current_url(expected_url)
--- tests/test_empty_courses_list.py
+++ tests/test_empty_courses_list.py
@@ -1,18 +1,13 @@
 import pytest
 from playwright.sync_api import sync_playwright, expect
 
+from pages.courses_list_page import CoursesListPage
+
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-        # Переходим на Courses с сохраненным ранее контекстом
-        chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-        # Проверяем элементы на Courses
-        expect(chromium_page_with_state).to_have_url('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-        courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_title).to_be_visible()
-        expect(courses_title).to_have_text('Courses')
-
-        result_block = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-        expect(result_block).to_be_visible()
-        expect(result_block).to_have_text('There is no results')
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_empty_view()
